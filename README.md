12c43a1a5001da26f861f662c4244a778983aeb1
Jason Ward <wmjasonward@gmail.com>
2017-12-14 12:50:14 -0600

    Initial commit in prep for npm

---
+++ README.md
@@ -0,0 +1,144 @@
+# jsftp-mlst
+Decorate JSFtp with some checksum support
+
+See:
+
+[JSFtp Homepage](https://github.com/sergi/jsftp "JSFtp Homepage")
+
+[RFC 3659 - Extensions to FTP](https://tools.ietf.org/html/rfc3659#page-23 "rfc3659")
+
+Be sure to check that the Ftp server supports MLST/MLSD feature before using this.
+
+If present in the MLST/MLSD response entries, `Create` and `Modify` facts are
+parsed and the attributes `create_dt` and `modify_dt`, respectively,
+are added to the entry object.
+`create_dt` and `modify_dt` are ISO 8601 combined date and time strings in UTC. 
+
+Fact names are normalized to lower case in the entry objects.
+
+#### Starting it up
+
+```javascript
+var JSFtp = require("jsftp");
+require('jsftp-mlst')(JSFtp);
+
+var Ftp = new JSFtp({
+  host: "myserver.com",
+  port: 3331, // defaults to 21
+  user: "user", // defaults to "anonymous"
+  pass: "1234" // defaults to "@anonymous"
+});
+```
+
+#### Added Methods
+
+##### Ftp.mlst(pathname, callback)
+
+With the `mlst` method you can retrieve the MLST entry for `pathname`. The method
+accepts a callback with the signature `err, entry`, in which `err` is the error
+response coming from the server (usually a 4xx or 5xx error code), or an error
+indicating the MLST response couldn't be parsed as expected, and `entry`
+is an object containing the entry facts returned by the server.
+
+Pathname is optional. If omitted, the server is expected to return
+an MLST entry for the current working directory.
+
+```javascript
+
+Ftp.mlst('myfile.txt', (err, entry) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(entry);
+    // Prints something like
+    // { pathname: '/myfile.txt',
+    //   modify_dt: '2017-06-14T20:06:11+00:00',
+    //   modify: '20170614200611',
+    //   perm: 'adfrw',
+    //   size: '2291365',
+    //   type: 'file',
+    //   unique: 'ca032380be1',
+    //   'unix.group': '501',
+    //   'unix.mode': '0644',
+    //  'unix.owner': '501' }
+  }
+});
+```
+
+##### Ftp.mlsd(pathname, callback)
+
+With the `mlsd` method you can retrieve the contents of the 
+directory specified with `pathname`. The `mlsd` method accepts a callback with the signature `err, entries`
+where `err` is the error response coming from the server or 
+error processing the server response, and `entries` is an 
+array of entry objects containing the facts returned by the server
+for each item in the directory (usually includes the dir itself).
+
+`pathname` is optional. If omitted, the server is expected to return the contents of the current working directory.
+
+```javascript
+Ftp.mlsd('/', (err, entries) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(entry);
+        // Prints something like
+        // [{ pathname: '/myfile.txt',
+        //   modify_dt: '2017-06-14T20:06:11+00:00',
+        //   modify: '20170614200611',
+        //   perm: 'adfrw',
+        //   size: '2291365',
+        //   type: 'file',
+        //   unique: 'ca032380be1',
+        //   'unix.group': '501',
+        //   'unix.mode': '0644',
+        //  'unix.owner': '501' },
+        // { pathname: '..',
+        //   modify_dt: '2017-06-14T20:06:19+00:00',
+        //   modify: '20170614200619',
+        //   perm: 'flcdmpe',
+        //   type: 'pdir',
+        //   unique: 'ca01u82148',
+        //   'unix.group': '501',
+        //   'unix.mode': '0700',
+        //   'unix.owner': '501' },
+        // { pathname: '.',
+        //   modify_dt: '2017-06-14T20:06:19+00:00',
+        //   modify: '20170614200619',
+        //   perm: 'flcdmpe',
+        //   type: 'cdir',
+        //   unique: 'ca01u82148',
+        //   'unix.group': '501',
+        //   'unix.mode': '0700',
+        //   'unix.owner': '501' }]
+  }
+});
+```
+
+##### Other Notes
+
+OS Dependent Facts have a '.' in their names (see 'unix.xxx' facts in examples above).
+If you need to read them you'll have to do something like:
+`entry['unix.mode']`
+
+MLSD/MLST implementations vary across Ftp servers. YMMV
+
+I tried to stick to the language support and eslint config of the
+jsftp project for consistency.
+
+##### To-Do
+
+I need to clean up the error handling around parsing of dates
+as well as any errors with individual entries of an MLSD response.
+Currently there are a couple of 'throws' in there which is inconsistent
+with the error handling pattern of jsftp.
+
+Add tests :) My current tests (not included in the repo) rely
+on a couple of remote ftp servers I control that support MLST. I need to
+create mock support for mlst/mlsd to test against.
+
+Figure out why Ftp.hasFeat doesn't seem to work. Not sure if there's
+an upstream problem or I'm just using it wrong. Ideally, you could
+call Ftp.hasFeat('MLST') to ensure the server supports the feature.
+As an alternative, create a `hasMlst` method that does a `Ftp.raw('feat'...)`
+and looks for MLST in the response.
---
+++ index.js
@@ -0,0 +1,193 @@
+"use strict";
+var once = require("once");
+var inherits = require("util").inherits;
+
+module.exports = function(jsftp) {
+
+  /**
+   * Helper function to convert an ftp time-val to a
+   * utc iso date string "YYYY-MM-DDTHH:MM:SS.nnn+00:00
+   *
+   * RFC 3659 allows for arbitrary precision for parts of a second
+   * this helper will only go to millisecond precision - anything else is rounded
+   *
+   * @param {string} timeval - The ftp time-val (probably from a mlst fact)
+   * @returns {string} The "iso" formatted string
+   */
+  function ftpTimeValToIsoDate(timeval) {
+    var parts = timeval.match(/^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\.\d+)?$/);
+    if (!(parts && parts.length > 0)) {
+      throw new Error("Unable to parse ftp time-val: " + timeval);
+    }
+
+    var millis = "";
+    if (parts[7]) {
+      millis = "." + Math.round((parseFloat(parts[7]) * 1000));
+    }
+
+    return `${parts[1]}-${parts[2]}-${parts[3]}T${parts[4]}:${parts[5]}:${parts[6]}${millis}+00:00`;
+  }
+
+  /**
+   * Parse and Mlst entry (and, by extension Mlsd entry)
+   *
+   * @param {string} entry entry line from ftp server (type=file;create=xxxxxxxxx;...)
+   * @returns {object} entry facts
+   */
+  function parseMlstEntry(entry) {
+    var parts = entry.trim().split(" "); // separate the facts from the pathname
+    if (parts.length > 2) {
+      if (parts[0].length === 0) {
+        parts.shift();  // in case we had a space at the beginning
+      } else {
+        throw new Error("Mlst/Mlsd entry has too many parts: {" + entry + "}");
+      }
+    }
+
+    var response = {};
+    if (parts.length > 1 && parts[1].length > 0) { // may not have a pathname if none was specified in command
+      response["pathname"] = parts[1];
+    }
+
+    var facts = parts[0].split(";");
+    for (var i = 0; i < facts.length; i++) {
+      var fe = facts[i].indexOf("="); // doing this way - rather than split - in case there can be an "=" in the fact value
+      if (fe > 0) { // if nothing before the = - not really usable anyway
+        var factname = facts[i].substr(0, fe).toLowerCase();
+        var factvalue = facts[i].substr(fe + 1).toLowerCase();
+
+        switch (factname) {
+          case "create":
+          case "modify":
+            response[factname + "_dt"] = ftpTimeValToIsoDate(factvalue);
+            break;
+        }
+        response[factname] = factvalue;
+      }
+    }
+
+    return response;
+  }
+
+  /**
+   * Executes MLST on the ftp server
+   *
+   * Caller should ensure MLST is supported by calling hasFeat("MLST") first
+   *
+   * @param {string} path Optional - The pathname to retrieve the mlst listing for
+   * @returns {object} mlst entry facts
+   *
+   */
+  jsftp.prototype.mlst = function(path, callback) {
+    if (arguments.length === 1) {
+      callback = arguments[0];
+      path = "";
+    }
+
+    var self = this;
+
+    this.raw("MLST " + path, function(err, response) {
+      if (err) {
+        callback(err);
+      } else {
+        // per the rfc, expecting a 250 response and a total of 3 lines
+        if (response.code === 250) {
+          var lines = response.text.split("\n"); // response parser converts crlf to lf
+          if (lines.length === 3) {
+            callback(null, parseMlstEntry(lines[1]));
+          } else {
+            callback({
+              Error: "Expected 3 lines in mlst command response",
+              code: response.code,
+              text: response.text,
+              isError: true,
+            }, null);
+          }
+        } else {
+          callback({
+            Error: "Expected 250 response from MLST command",
+            code: response.code,
+            text: response.text,
+            isError: true,
+          }, null);
+        }
+      }
+    });
+  };
+
+  /**
+   * Calls MLSD to list a path using a passive connection.
+   * See <a href="https://tools.ietf.org/html/rfc3659">RFC 3659</a>
+   *
+   * Requires feature "MLST" - jsftp.hasFeat("MLST")
+   *
+   * If successful, the callback will be passed an array of entry objects
+   *
+   * This follows the pattern in jsftp.list
+   *
+   * @param {String} path Optional Remote path to list using MLSD
+   * @param {Function} callback Function to call with errors or results
+   */
+  jsftp.prototype.mlsd = function(path, callback) {
+    if (arguments.length === 1) {
+      callback = arguments[0];
+      path = "";
+    }
+
+    var self = this;
+    var data = ""; // from data connection
+    callback = once(callback);
+
+    self.getPasvSocket(function(err, socket) {
+      if (err) {
+        return callback(err);
+      }
+
+      socket.setEncoding("utf8");
+      socket.on("data", function(d) {
+        data += d;
+      });
+
+      self.pasvTimeout(socket, callback);
+
+      socket.once("close", function(err) {
+
+        var lines = data.split("\r\n");
+        var entries = [];
+        for (var i = 0; i < lines.length; i++) {
+          if (lines[i].length > 0) {
+            entries.push(parseMlstEntry(lines[i]));
+          }
+        }
+        callback(err, entries);
+      });
+
+      socket.once("error", callback);
+
+      function cmdCallback(err, res) {
+        if (err) {
+          return callback(err);
+        }
+
+        var isExpectedMark = this.expectedMarks.some(function(mark) {
+          return mark === res.code;
+        });
+
+        if (!isExpectedMark) {
+          callback(new Error(
+            "Expected marks " + this.expectedMarks.toString() + " instead of: " +
+            res.text));
+        }
+      }
+
+      cmdCallback.expectedMarks = {
+        marks: [150],
+        ignore: 226
+      };
+
+      self.execute("MLSD " + (path || ""), cmdCallback);
+    });
+  };
+
+  return jsftp;
+};
